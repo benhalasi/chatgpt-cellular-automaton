0a3988f890c337b3adbd4d98dc41e58762b0bb39
Benedek Halasi <t0h3ow7t2@mozmail.com>
2023-01-03 10:49:03 +0100

    add script that exports chatGPT convo as html

---
+++ docs/export-chat.js
@@ -0,0 +1,37 @@
+(() => {
+  const mainNode = $('main')
+  const text = [
+    '<!DOCTYPE html>',
+    '<head>',
+    '<!-- generated: ' + new Date().toISOString() + ' -->',
+    '<!-- answares: ' + $$('.markdown').length + ' -->',
+    '<!-- title: ' + document.title + ' -->',
+    '<title>saved: ' + document.title + '</title>',
+    '<script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>',
+    '<style>',
+    '  button, form { display: none !important; }',
+    '</style>',
+    '</head>',
+    '<body>',
+    '<main class="' + mainNode.className + '">',
+    mainNode.innerHTML,
+    '</main>',
+    '</body>',
+  ]
+  const textBlob = new Blob(text, {
+    type: 'text/html',
+  });
+  const downloadAnchor = document.createElement('a');
+  downloadAnchor.download =
+    [
+      new Date().toISOString().split(':').splice(0,2).join('').split('T').join(' '),
+     'ChatGPT',
+     document.title,
+     // new Date().toLocaleString()
+     // new Date().toISOString().split('T')[0]
+    ]
+    .join(' - ')
+    .concat('.html');
+  downloadAnchor.href = window.URL.createObjectURL(textBlob);
+  downloadAnchor.click();
+})()
